c828f4db5f8231be1de721608abba60caa0a9460
Ercan Öztürk <ercanozturk788@gmail.com>
2024-03-11 17:02:51 +0300

    User registration

--- DjangoProject/DjangoProject/settings.py
+++ DjangoProject/DjangoProject/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
+    'crispy_forms',
+    "crispy_bootstrap5",
     'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -122,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
--- DjangoProject/blog/views.py
+++ DjangoProject/blog/views.py
@@ -1,22 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'Ercan',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date': '8 March, 2024'
-    },
-{
-        'author': 'Ercan',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date': '9 March, 2024'
-    },
-]
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 def about(request):
